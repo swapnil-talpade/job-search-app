fdbb529c190568c1b3e7893b332753d08710f2fd
swapnil-talpade <swapniltalpade41@gmail.com>
2023-05-11 12:03:32 +0530

    feat: introduce job details page

---
+++ app/job-details/[id].js
@@ -0,0 +1,76 @@
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import ScreenHeaderBtn from "../../components/common/header/ScreenHeaderBtn";
+import Company from "../../components/jobdetails/company/Company";
+import Tabs from "../../components/jobdetails/tabs/Tabs";
+import { COLORS, SIZES, icons } from "../../constants";
+import { dummyData } from "../../data/data";
+
+const JobDetails = () => {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const isLoading = false;
+  const error = false;
+
+  const onRefresh = () => {};
+
+  // const { isLoading, error, data, refetch} = useFetch("job-details", {
+  //   job_id: params.id,
+  //   num_pages: 1,
+  // });
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension={"60%"}
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.share} dimension={"60%"} />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {isLoading ? (
+          <ActivityIndicator size={"large"} color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : dummyData.length === 0 ? (
+          <Text>No data</Text>
+        ) : (
+          <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+            <Company />
+            <Tabs />
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default JobDetails;
